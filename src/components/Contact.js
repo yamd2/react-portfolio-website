47b4e7a8e56ada511ca6e2ad2b1d268d21cc3e46
Yam Dangar <yamdangar@gmail.com>
2022-11-07 11:20:34 +1100

    multiple page application

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Layout } from "./Layout";
 
 export const Contact = () => {
   return (
-    <Container id="contact" className=" contact rounded-5 py-5">
-      <div className="title">
-        <span>Contact Me</span>
-      </div>
-      <Row className=" fa-3x">
-        <Col className="  d-flex justify-content-around pb-4">
-          <a href="tel:0409876789">
-            <i className="fa-solid fa-mobile"></i>
-          </a>
-          <a href="mailto:yor@email.com">
-            <i className="fa-solid fa-envelope"></i>
-          </a>
-          <a href="##">
-            {" "}
-            <i className="fa-brands fa-facebook"></i>
-          </a>
-          <a href="##">
-            {" "}
-            <i className="fa-brands fa-linkedin"></i>
-          </a>
-          <a href="##">
-            <i className="fa-brands fa-square-instagram"></i>
-          </a>
-        </Col>
-      </Row>
-    </Container>
+    <Layout>
+      <Container id="contact" className=" contact rounded-5 py-5 mt-5">
+        <div className="title">
+          <span>Contact Me</span>
+        </div>
+        <Row className=" fa-3x">
+          <Col className="  d-flex justify-content-around pb-4">
+            <a href="tel:0409876789">
+              <i className="fa-solid fa-mobile"></i>
+            </a>
+            <a href="mailto:yor@email.com">
+              <i className="fa-solid fa-envelope"></i>
+            </a>
+            <a href="##">
+              {" "}
+              <i className="fa-brands fa-facebook"></i>
+            </a>
+            <a href="##">
+              {" "}
+              <i className="fa-brands fa-linkedin"></i>
+            </a>
+            <a href="##">
+              <i className="fa-brands fa-square-instagram"></i>
+            </a>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 };
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,34 +1,38 @@
 import React from "react";
 import { Col, Container, Row } from "react-bootstrap";
+import { Layout } from "./Layout";
 
 export const Skills = () => {
   return (
-    <Container id="skills" className=" py-3 mb-5">
-      <Row>
-        <Col>
-          {/* <!-- title --> */}
-          <div className="title">
-            <span>Skills</span>
-          </div>
+    <Layout>
+      <Container id="skills" className=" py-3 mb-5">
+        <Row>
+          <Col>
+            {/* <!-- title --> */}
+            <div className="title">
+              <span>Skills</span>
+            </div>
 
-          {/* <!-- icons --> */}
+            {/* <!-- icons --> */}
 
-          <div className="icons">
-            <div>
-              <i className="fa-brands fa-html5 text-danger"></i> HTML
+            <div className="icons">
+              <div>
+                <i className="fa-brands fa-html5 text-danger"></i> HTML
+              </div>
+              <div>
+                <i className="fa-brands fa-css3-alt text-primary"></i> CSS
+              </div>
+              <div>
+                <i className="fa-brands fa-square-js text-warning"></i>{" "}
+                JavaScript
+              </div>
+              <div>
+                <i className="fa-brands fa-figma text-info"></i> Figma
+              </div>
             </div>
-            <div>
-              <i className="fa-brands fa-css3-alt text-primary"></i> CSS
-            </div>
-            <div>
-              <i className="fa-brands fa-square-js text-warning"></i> JavaScript
-            </div>
-            <div>
-              <i className="fa-brands fa-figma text-info"></i> Figma
-            </div>
-          </div>
-        </Col>
-      </Row>
-    </Container>
+          </Col>
+        </Row>
+      </Container>
+    </Layout>
   );
 };
